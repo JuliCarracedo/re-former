e741faf295d1fb363025717543b4fd0b840bd51b
vicmaburrito <vmaldaraca@gmail.com>
2021-06-10 20:20:42 -0500

    Add linter

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class User < ApplicationRecord
   validates :username, presence: true, length: { maximum: 15 },
                        format: { with: /\A[\w-]+\z/, message: 'your name requirements' }
