a8e757ece7022101d965c640f18d9dfcb4d8d646
vicmaburrito <vmaldaraca@gmail.com>
2021-06-10 20:07:11 -0500

    Fix controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,15 +2,14 @@ class UsersController < ApplicationController
     def index
       @users = User.all
     end
-    
     def new
         @user = User.new
     end
-
     def create
         @user = User.new(user_params)
+
         if @user.save
-          redirect_to root_path
+          redirect_to @user
         else
           render :new
         end
